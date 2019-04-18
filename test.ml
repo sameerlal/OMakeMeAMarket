8a0561a39278eb0145edc616be76de2ba501fcc3
Shikhor Shams Wahed <ssw77@cornell.edu>
2019-04-17 21:54:33 -0400

    tradertest

--- test.ml
+++ test.ml
@@ -2,6 +2,10 @@
    TESTING SUITE 
  ********************************************************************)
 open OUnit2
+open Trader
+open Marketmaker
+open Command
+open Parse
 
 let suite = "CamlCoin Test suite" >::: []
 
@@ -28,10 +32,40 @@ let make_to_float_list_test
     (lst : string list)
     (expected_output : float list): test =
   name >:: (fun _ -> assert_equal expected_output (Stats.to_float_list lst)
-  ~printer: (pp_list pp_float))
+               ~printer: (pp_list pp_float))
 
 let to_float_list_tests = [
   make_to_float_list_test "to float test 1: single elt" ["1.5"] [1.5];
+]
+(*Trader's types *)
+let sample_bidask = {
+  bid = 10;
+  ask = 11;
+  spread = 1
+}
+
+let sample_transaction = {
+  timestamp = 1;
+  bidask = sample_bidask;
+  order_type = "hit"; (* bid or ask *)
+}
+
+let sample_orderbook = {
+  transactions = [sample_transaction];
+  buys = 1;
+  sells = 0;
+}
+
+let sample_trader = {
+  true_value = 125;
+  avg_buy_value = 25;
+  profit = 0 ;
+  cash = 200;
+  inventory = 4; (* Total number of shares owned *)
+  orderbook = sample_orderbook;
+}
+let trader_tests = [
+
 ]
 
 let suite = 

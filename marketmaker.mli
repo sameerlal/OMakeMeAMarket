6ad68c1d89b83e92f1ef13a4a487e7f187370c88
Ryan Richardson <rrr225@cornell.edu>
2019-04-24 20:43:15 -0400

    mli spec + some extra specs

--- marketmaker.mli
+++ marketmaker.mli
@@ -42,14 +42,56 @@ type receive_transaction =  {
   transaction : bidask;
 }
 
+(**[init_market game] is the initial state of the market after a [game] is 
+   started. It initializes all fields to its starting values and marks
+   the "trade_type" field in the record as "init".  *)
 val init_market : string -> t
+
+(**[get_timestamp market] is the value of the timestamp in the market  *)
 val get_timestamp : t -> int
+
+(**[transaction market trade] is the new state of the marketmaker after a trade.
+   It takes in a type t [market] and a [trade] and sets a new inventory, 
+   bidask, timestamp, orderbook and curr_profit for the marketmaker.
+   We update the timestamp here.
+*)
 val transaction : receive_transaction -> t -> t
+
+(**[generate_receive_transaction timestamp trade_type bid ask] returns a 
+   transaction variant with the given arguments. *)
 val generate_receive_transaction : int -> string -> int -> int -> 
   receive_transaction
+
+(**[stringify_bid_ask market] is a string of the current bid and ask. *)
 val stringify_bid_ask : t -> string
+
+(**  [get_outstandingshares market] returns the outstanding shares of the 
+     market maker's state [market]. *)
 val get_outstandingshares : t -> int
+
+(**[increment_timestep market] is a type t [market] with the timestamp 
+   incremented. *)
 val increment_timestep : t -> t
+
+(**[display_data state] is a unit with side-effects of displaying data about 
+   transactions and the currents status of the player and market. 
+
+   This uses ANSITerminal to pretty-print the market maker's statistics.
+   It currently includes the following data:
+   - Current Bid/Ask
+   - Spread
+   - Timestamp
+   - Current Profit
+   - # Coins accumulated
+*)
 val display_data : t -> unit
+
+(**  [stringify_bidask_history market] returns a history of past market markets
+     as seen in [market] state.  *)
 val stringify_bidask_history : t -> unit
+
+(** [exchange_mm_excess market sum_dice] will return a new market.t object 
+    with excess/deficient shares of camlcoin swapped for cash in accordance 
+    with the true market value, [sum_dice].  The final excess coins will be 0 
+    (invariant).  *)
 val exchange_mm_excess : t -> int -> t
--- parse.mli
+++ parse.mli
@@ -16,11 +16,29 @@ val fermi_json : t
 val oth_situation : situation
 val first_situation : situation
 (* END *)
+
+(**[from_json json] is a type t with values from the [json] file. *)
 val from_json : Yojson.Basic.t -> t
+
+(** [get_question data] returns a question from the [data] variant *)
 val get_question : t -> string
+
+(** [get_answer data] returns an answer from the [data] variant *)
 val get_answer : t -> string
+
+(* [get_nth_situation index data] is the nth situation from json. *)
 val get_nth_situation : int -> t -> situation
+
+(* [get_event_from_situation sit] is the event from a situation.  *)
 val get_event_from_situation: situation -> string
+
+(* [get_effect_from_situation sit] is the effect from a situation. It's an 
+   option indicating if it it causes the price to rise or to fall *)
 val get_effect_from_situation : situation -> (string * string) option
+
+(**[get_intro data] results in the ascii introduction string being printed from 
+   the json. *)
 val get_intro : t -> unit
+
+(** [show_dice n] returns a string indicating a dice with visible face [n]. *)
 val show_dice : int -> string
